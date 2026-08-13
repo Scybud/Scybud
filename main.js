b3e7b4e849411bd99d59ba096b5d2f91f9b5e683
scybudtech <scybudtech@gmail.com>
2026-08-13 16:48:02 +0200

    Refactor navigation structure: Update sidebar and dropdown components for improved accessibility and UI consistency

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const sidebar = document.getElementById("sidebar");
 function togglesidebar() {
   sidebar.classList.toggle("show");
 }
+
+const dropdownOpen = document.getElementById("dropdownOpen");
+const dropdown = document.querySelector(".dropdown");
+if(dropdownOpen && dropdown) {
+
+  dropdownOpen.addEventListener("click", () => {
+    dropdown.classList.toggle("drop");
+  });
+}
